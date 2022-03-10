12743bc0c1b6525b86a7e6114bd0b2d6426000aa
Assandri Valentino (HRE-JMP2-DE IOB/PAD) <asv1sh@bosch.com>
2022-03-10 14:00:47 +0100

    changed port

--- stage_three/main.py
+++ stage_three/main.py
@@ -2,14 +2,20 @@
 This module defines what will happen in stage_3.
 """
 from flask import Flask, jsonify, make_response
+import pandas as pd
 
 app = Flask(__name__)
 
 
 @app.route("/api/v1/evaluate", methods=["GET", "POST"])
 def evaluate():
+    getLogs()
     return make_response(jsonify({"value": 3.1415926535}))
+
+def getLogs():
+    df = pd.load_csv('data/logs.csv')
+    print(df)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=6000)
