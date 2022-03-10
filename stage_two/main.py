4cb72cacad18eadb5a7ee4146ed1cadd5e3cb605
Assandri Valentino (HRE-JMP2-DE IOB/PAD) <asv1sh@bosch.com>
2022-03-10 13:50:15 +0100

    saving to a csv in data folder

--- stage_two/main.py
+++ stage_two/main.py
@@ -2,11 +2,18 @@
 This module defines what will happen in stage_2.
 """
 import requests
-
+import pandas as pd
+from datetime import datetime
 
 def main() -> None:
     response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
     print(response.text)
+    dt = datetime.now()
+    df = pd.DataFrame({'time': [dt],
+                   'action': ['Run Stage 2' ],
+                   'output': [response.text]
+                   })
+    df.to_csv('data/logs.csv', mode='a', index=False, header=False)
 
 
 if __name__ == "__main__":
