f7f03445b46255dbfb2696ee1612e6ac274df6f0
Assandri Valentino (HRE-JMP2-DE IOB/PAD) <asv1sh@bosch.com>
2022-03-11 09:45:12 +0100

    add print cwd

--- stage_two/main.py
+++ stage_two/main.py
@@ -4,13 +4,15 @@ This module defines what will happen in stage_2.
 from datetime import datetime
 import requests
 import pandas as pd
+import os
 
 
 
 def main() -> None:
     response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
     print(response.text)
-
+    CURR_DIR = os.getcwd()
+    print(CURR_DIR)
     timestamp = datetime.now()
     log_df = pd.DataFrame({'time': [timestamp],
                    'action': ['Run Stage 2']})
