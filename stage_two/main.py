d57b7fdd77cc27d76239205f64ca0295d54de6f7
Assandri Valentino (HRE-JMP2-DE IOB/PAD) <asv1sh@bosch.com>
2022-03-11 09:58:28 +0100

    not saving csv for now

--- stage_two/main.py
+++ stage_two/main.py
@@ -2,9 +2,10 @@
 This module defines what will happen in stage_2.
 """
 from datetime import datetime
+import os
 import requests
 import pandas as pd
-import os
+
 
 
 
@@ -19,7 +20,7 @@ def main() -> None:
     timestamp = datetime.now()
     log_df = pd.DataFrame({'time': [timestamp],
                    'action': ['Run Stage 2']})
-    log_df.to_csv('data/logs.csv', mode='a', index=False, header=False)
+    #log_df.to_csv('data/logs.csv', mode='a', index=False, header=False)
 
 
 if __name__ == "__main__":
